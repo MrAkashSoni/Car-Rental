becbdbc7fbd3646a56284b25df7d3643e45b0ace
MrAkashSoni <mr.akashsoni10@gmail.com>
2020-04-06 20:22:18 +0530

    commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect('mongodb+srv://Car-Rental:root@cluster0-mby8m.mongodb.net/car-r
   useCreateIndex: true,
   useFindAndModify: false
 }).then(() => {
-  console.log("cloud Connection Done");
+  console.log("Connection Done");
 });
 
 mongoose.connect('mongodb://localhost:27017/car-rental_repo', {
